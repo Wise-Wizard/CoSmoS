4897aaaae731727b5f4eed27a6d0d4adda3c63bd
Wise-Wizard <saransh.shankar@gmail.com>
2022-08-23 19:44:12 +0530

    Home Screen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/apod_screen.dart';
 import 'package:flutter_application_1/Screens/mars_rover_screen.dart';
@@ -19,12 +20,61 @@ class HomeScreen extends StatelessWidget {
         child: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ElevatedButton(
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TyperAnimatedText('Welcome To  ',
+                        speed: const Duration(milliseconds: 100),
+                        textStyle: const TextStyle(fontSize: 60)),
+                  ],
+                  repeatForever: true,
+                  pause: const Duration(seconds: 5),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TyperAnimatedText('Our CoSmoS!!',
+                        speed: const Duration(milliseconds: 100),
+                        textStyle: const TextStyle(fontSize: 60)),
+                  ],
+                  repeatForever: true,
+                  pause: const Duration(seconds: 5),
+                ),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, APODScreen.id);
+                  },
+                  color: const Color(0xFF94908D),
+                  child: const Text(
+                    'Nasa\'s APOD',
+                    style: TextStyle(fontSize: 45, color: Color(0xFF53354A)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: MaterialButton(
                   onPressed: () {
                     Navigator.pushNamed(context, MarsRover.id);
                   },
-                  child: Text('APODScreen'))
+                  color: const Color(0xFF94908D),
+                  child: const Text(
+                    'Mars Rover',
+                    style: TextStyle(color: Color(0xFF53354A), fontSize: 45),
+                  ),
+                ),
+              )
             ],
           ),
         ),
